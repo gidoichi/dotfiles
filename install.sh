8ccf45db3410cf70de88ad98847165e0dc7f7045
Shinichi Yanagido <s.yanagido@gmail.com>
2022-06-11 12:29:30 +0900

    echo linked files

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PARENT="$(d=${0%/*}/; [ "_$d" = "_$0/" ] && d='./'; cd "$d"; pwd)"
 find "${PARENT}/" -mindepth 1 -maxdepth 1 -name '.*' -not -name '.git' |
 sed 's#^.*/##' |
 while IFS= read -r file; do
-    (set -x
-     ln -s "${PARENT}/${file}" "${HOME}/" "${@}"
-    )
+    if ! (set -x; ln -s "${PARENT}/${file}" "${HOME}/" "${@}"); then
+        ls -ld "${HOME}/${file}"
+    fi
 done
