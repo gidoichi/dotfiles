9419207556fc86b76870da42b0522391b3b19879
Shinichi Yanagido <s.yanagido@gmail.com>
2022-06-14 08:11:22 +0900

    add env for homebrew

--- .shell/init.d/01-env-vars.sh
+++ .shell/init.d/01-env-vars.sh
@@ -1,35 +1,41 @@
 #!/bin/sh
 
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.shell/bin:$PATH"
+export PATH="${HOME}/.local/bin:${PATH}"
+export PATH="${HOME}/.shell/bin:${PATH}"
 
 if type emacs >/dev/null 2>&1; then
     export EDITOR=emacs
 fi
 
-if [ -d "$HOME/.emacs.d/bin" ]; then
-    export PATH="$HOME/.emacs.d/bin:$PATH"
+if [ -d "${HOME}/.emacs.d/bin" ]; then
+    export PATH="${HOME}/.emacs.d/bin:${PATH}"
 fi
 
 if type go >/dev/null 2>&1; then
-    export GOPATH="$HOME/go"
-    export PATH="$GOPATH/bin:$PATH"
+    export GOPATH="${GOPATH:-${HOME}/go}"
+    export PATH="${GOPATH}/bin:${PATH}"
 fi
 
+export NPM_HOME="${NPM_HOME:-${HOME}/.npm}"
 if type npm >/dev/null 2>&1; then
-    export PATH="$HOME/.npm/bin:$PATH"
+    export PATH="${NPM_HOME}/bin:${PATH}"
 fi
 
 export LC_COLLATE=ja_JP.UTF-8
 
 if type screen >/dev/null 2>&1; then
-    export SCREENDIR="$HOME/.screen"
+    export SCREENDIR="${HOME}/.screen"
 fi
 
 if type fzf >/dev/null 2>&1; then
     export LINE_SELECTOR=fzf
 elif type lnum-select >/dev/null 2>&1; then
     export LINE_SELECTOR=lnum-select
+fi
+
+export HOMEBREW_HOME="${HOMEBREW_HOME:-${HOME}/.homebrew}"
+if [ -d "${HOMEBREW_HOME}/bin" ]; then
+    export PATH="${HOMEBREW_HOME}/bin:${PATH}"
 fi
 
 export ONELINE_SELECTOR=1line-select
