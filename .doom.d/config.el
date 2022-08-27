7803586f5502a0f67c2564e17370334b54fd5822
Shinichi Yanagido <s.yanagido@gmail.com>
2022-08-28 00:04:49 +0900

    refactor config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -77,9 +77,10 @@
 
 (setq confirm-kill-emacs nil)
 
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+(use-package! server
+  :config
+  (unless (server-running-p)
+  (server-start)))
 
 (setq tab-bar-show 1)
 (unless window-system
@@ -108,34 +109,6 @@
       "M-%" 'anzu-query-replace
       "C-M-%" 'anzu-query-replace-regexp)
 
-(use-package! flycheck
-  :config
-  (if (/= 0 (call-process-shell-command "type textlint"))
-      (display-warning load-file-name "cannot find 'textlint' command to use 'flycheck-define-checker textlint'")
-    (setq textlint-config-file (expand-file-name ".textlintrc" (getenv "HOME")))
-    (flycheck-define-checker textlint
-      "A linter for Markdown."
-      :command ("textlint" "--format" "unix" "--config" (eval textlint-config-file) source)
-      :error-patterns
-      ((warning line-start (file-name) ":" line ":" column ": "
-                (message (minimal-match (one-or-more anychar)))
-                "[" (id (one-or-more (not ?\s))) "]"
-                line-end))
-      :modes (text-mode markdown-mode gfm-mode))
-    ))
-
-(use-package! helm-ghq
-  :config
-  (defalias 'ghq 'helm-ghq))
-
-(use-package! helm-git-grep
-  :config
-  (defalias 'git-grep 'helm-git-grep))
-
-(use-package! helm-ls-git
-  :config
-  (defalias 'git-ls-files 'helm-ls-git))
-
 (use-package! centaur-tabs
   :hook
   (special-mode . centaur-tabs-mode)
@@ -154,6 +127,22 @@
   (unless window-system
     (eaw-fullwidth)))
 
+(use-package! flycheck
+  :config
+  (if (/= 0 (call-process-shell-command "type textlint"))
+      (display-warning load-file-name "cannot find 'textlint' command to use 'flycheck-define-checker textlint'")
+    (setq textlint-config-file (expand-file-name ".textlintrc" (getenv "HOME")))
+    (flycheck-define-checker textlint
+      "A linter for Markdown."
+      :command ("textlint" "--format" "unix" "--config" (eval textlint-config-file) source)
+      :error-patterns
+      ((warning line-start (file-name) ":" line ":" column ": "
+                (message (minimal-match (one-or-more anychar)))
+                "[" (id (one-or-more (not ?\s))) "]"
+                line-end))
+      :modes (text-mode markdown-mode gfm-mode))
+    ))
+
 (use-package! helm
   :config
   (setq helm-grep-file-path-style 'relative)
@@ -161,16 +150,17 @@
       (display-warning load-file-name "cannot find 'ag' command to use 'helm-do-grep-ag'")
     (defalias 'find-grep 'helm-do-grep-ag)))
 
-(use-package! treemacs
+(use-package! helm-ghq
   :config
-  (setq doom-themes-treemacs-theme "doom-colors")
-  (pcase (cons (not (null (executable-find "git")))
-               (not (null treemacs-python-executable)))
-    (`(t . t) (treemacs-git-mode 'deferred))
-    (`(t . _) (treemacs-git-mode 'simple)))
-  (treemacs-follow-mode t)
-  (treemacs-project-follow-mode t)
-  )
+  (defalias 'ghq 'helm-ghq))
+
+(use-package! helm-git-grep
+  :config
+  (defalias 'git-grep 'helm-git-grep))
+
+(use-package! helm-ls-git
+  :config
+  (defalias 'git-ls-files 'helm-ls-git))
 
 (map! :mode special-mode
       "q" 'kill-buffer-and-window)
@@ -186,6 +176,16 @@
     (setq word-wrap nil)
     (global-visual-line-mode -1)))
 
+(use-package! treemacs
+  :config
+  (setq doom-themes-treemacs-theme "doom-colors")
+  (pcase (cons (not (null (executable-find "git")))
+               (not (null treemacs-python-executable)))
+    (`(t . t) (treemacs-git-mode 'deferred))
+    (`(t . _) (treemacs-git-mode 'simple)))
+  (treemacs-follow-mode t)
+  (treemacs-project-follow-mode t)
+  )
 
 (use-package! undo-tree
   :hook
