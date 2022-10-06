3837cf19d12a9a5f36468f6bf675b1d2cf61e1dd
Shinichi Yanagido <s.yanagido@gmail.com>
2022-10-06 20:44:29 +0900

    remove unnecessary code

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ IFS="${IFS%_}"
 rc=0
 for file in $(find "${PARENT}/" -mindepth 1 -maxdepth 1 -name '.*' -not -name '.git' |
                sed 's#^.*/##'); do
-    if [ -L "${HOME}/${file}" ] >/dev/null; then
+    if [ -L "${HOME}/${file}" ]; then
         inode_org="$(ls -di "${PARENT}/${file}" | cut -d ' ' -f 1)"
         inode_lnk="$(ls -di "$(readlink "${HOME}/${file}")" | cut -d ' ' -f 1)"
         if [ "${inode_org}" -eq "${inode_lnk}" ]; then
