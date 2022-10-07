7b936dda2349ebd254c1d4f23432ddbd7fd634ac
Shinichi Yanagido <hakuto1yds@gmail.com>
2022-10-08 00:32:26 +0900

    return 1 on git_credential_keepassxc error

--- .shell/init.d/45-keepassxc.sh
+++ .shell/init.d/45-keepassxc.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
 if type git-credential-keepassxc.exe >/dev/null; then
-    git_credential_keepassxc() (
-        printf "url=%s\nusername=%s\n" "${1}" "${HOST}" |
-            git-credential-keepassxc.exe --unlock 0 get --json |
-            jq -r '.password'
-    )
+    git_credential_keepassxc() {
+        if ! cred=$(printf "url=%s\nusername=%s\n" "${1}" "${HOST}" |
+                        git-credential-keepassxc.exe --unlock 0 get --json)
+        then
+            return 1
+        fi
+        printf '%s' "${cred}" | jq -r '.password'
+    }
 elif type git-credential-keepassxc >/dev/null; then 
-    git_credential_keepassxc() (
-        printf "url=%s\nusername=%s\n" "${1}" "${HOST}" |
-            git-credential-keepassxc --unlock 0 get --json |
-            jq -r '.password'
-    )
+    git_credential_keepassxc() {
+        if ! cred=$(printf "url=%s\nusername=%s\n" "${1}" "${HOST}" |
+                        git-credential-keepassxc --unlock 0 get --json)
+        then
+            return 1
+        fi
+        printf '%s' "${cred}" | jq -r '.password'
+    }
 fi
 
 
