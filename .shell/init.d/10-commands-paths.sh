e17c5f4170ab16d693e6245d61036b5a151a85fd
gidoichi <hakuto1yds@gmail.com>
2025-04-24 17:32:28 +0900

    tweak

--- .shell/init.d/10-commands-paths.sh
+++ .shell/init.d/10-commands-paths.sh
@@ -38,5 +38,5 @@ if [ -d "${_target}" ]; then
 fi
 
 if type asdf >/dev/null 2>&1; then
-    export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+    export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:${PATH}"
 fi
