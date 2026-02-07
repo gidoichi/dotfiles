6b191da722e3a395fe88bd28beac812dce9da271
gidoichi <hakuto1yds@gmail.com>
2026-02-07 11:37:35 +0900

    .shell: fix paths for homebrew

--- .shell/init.d/10-commands-paths.sh
+++ .shell/init.d/10-commands-paths.sh
@@ -20,6 +20,8 @@ if type ghq >/dev/null; then
     GHQ_ROOT="$(ghq root)"
 fi
 
+_target='/opt/homebrew/bin'
+if [ -d "${_target}" ]; then export PATH="${PATH}:${_target}"; fi
 _target="/home/linuxbrew/.linuxbrew/bin"
 if [ -d "${_target}" ]; then export PATH="${PATH}:${_target}"; fi
 if type brew >/dev/null 2>&1; then
